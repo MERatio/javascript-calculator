0276fcbba95354a51d3bf1bd6a48459433771d26
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-09-21 22:18:39 +0800

    Add a func that uses the operations obj

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 'use strict';
 
+let operator = '*';
+let firstNumber = 2;
+let secondNumber = 6;
+
 const operations = {
 	add: (a, b) => a + b,
 	subtract: (a, b) => a - b,
 	multiply: (a, b) => a * b,
 	divide: (a, b) => a / b,
 };
+
+function operate(operator, a, b) {
+	switch (operator) {
+		case '+':
+			return operations.add(a, b);
+		case '-':
+			return operations.subtract(a, b);
+		case '*':
+			return operations.multiply(a, b);
+		case '/':
+			return operations.divide(a, b);
+	}
+}
+
+console.log(operate(operator, firstNumber, secondNumber));
