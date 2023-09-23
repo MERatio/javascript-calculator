14a300bd74d1094b2c9f3fb6490c732c08334264
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-09-23 13:49:23 +0800

    Calculate numbers, and fix display scroll bar

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 'use strict';
 
 const dom = {
+	display: document.querySelector('.display'),
 	displayText: document.querySelector('.display-text'),
+	operators: document.querySelectorAll('.operator'),
 	numbers: document.querySelectorAll('.number'),
-	updateDisplayText(str) {
-		if (this.displayText.textContent === '0') {
-			this.displayText.textContent = str;
-		} else {
-			this.displayText.textContent += str;
+	activateOperator(operator) {
+		const prevActiveDomOperator = document.querySelector('.active-operator');
+		const domOperator = document.querySelector(`[data-key="${operator}"]`);
+		if (prevActiveDomOperator) {
+			prevActiveDomOperator.classList.remove('active-operator');
+		}
+		if (domOperator) {
+			domOperator.classList.add('active-operator');
 		}
 	},
+	updateDisplayText(text) {
+		this.displayText.textContent = text;
+		// Make the scroll flush to the right
+		this.display.scrollLeft = this.display.scrollWidth;
+	},
+	equals: document.querySelector('.equals'),
 };
 
-let operator = '*';
-let firstNumber = 2;
-let secondNumber = 6;
+let firstNumber = 0;
+let operator = null;
+let secondNumber = null;
+let resultComputedWithEquals = false;
 
 const operations = {
 	add: (a, b) => a + b,
@@ -36,9 +48,68 @@ function operate(operator, a, b) {
 	}
 }
 
-dom.numbers.forEach((number) => {
-	number.addEventListener('click', (e) => {
+function determineNewDisplayText(key) {
+	const displayText = document.querySelector('.display-text').textContent;
+	let newDisplayText;
+	if (
+		displayText === '0' ||
+		resultComputedWithEquals ||
+		(operator !== null && secondNumber === null)
+	) {
+		newDisplayText = key;
+		resultComputedWithEquals = false;
+	} else {
+		newDisplayText = dom.displayText.textContent + key;
+	}
+
+	return newDisplayText;
+}
+
+dom.operators.forEach((domOperator) => {
+	domOperator.addEventListener('click', (e) => {
 		const key = e.currentTarget.dataset.key;
-		dom.updateDisplayText(key);
+
+		if (operator === null && secondNumber === null) {
+			operator = key;
+			dom.activateOperator(key);
+		} else if (operator !== null && secondNumber !== null) {
+			const result = operate(operator, firstNumber, secondNumber);
+			dom.updateDisplayText(result);
+			firstNumber = result;
+			operator = key;
+			dom.activateOperator(key);
+			secondNumber = null;
+		}
 	});
+});
+
+dom.numbers.forEach((domNumber) => {
+	domNumber.addEventListener('click', (e) => {
+		const key = e.currentTarget.dataset.key;
+		const newDisplayText = determineNewDisplayText(key);
+		const newNumber = Number.parseFloat(newDisplayText);
+
+		if (operator === null || resultComputedWithEquals) {
+			firstNumber = newNumber;
+			resultComputedWithEquals = false;
+		} else {
+			secondNumber = newNumber;
+		}
+
+		dom.updateDisplayText(newDisplayText);
+	});
+});
+
+dom.equals.addEventListener('click', () => {
+	if (operator !== null && secondNumber !== null) {
+		const displayText = dom.displayText.textContent;
+		secondNumber = Number.parseFloat(displayText);
+		const result = operate(operator, firstNumber, secondNumber);
+		firstNumber = result;
+		operator = null;
+		dom.activateOperator(operator);
+		secondNumber = null;
+		dom.updateDisplayText(result);
+		resultComputedWithEquals = true;
+	}
 });
